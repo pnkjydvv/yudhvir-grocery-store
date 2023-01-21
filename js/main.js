8272f0cedb0f9de70bae97b99d23d681c0ceaef9
Pankaj Yadav <pk469349@gmail.com>
2023-01-21 21:19:49 +0530

    first commit
    test

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "275px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
+
+
